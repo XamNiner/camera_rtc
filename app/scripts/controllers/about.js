1268e00b8a5f2f2cb9f9ac21721fc69346497cfe
XamNiner <praxis@praxiss-Mac-Pro.local>
2016-06-20 14:59:31 +0200

    test

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cameraRtcApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cameraRtcApp
+ */
+angular.module('cameraRtcApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cameraRtcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cameraRtcApp
+ */
+angular.module('cameraRtcApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
