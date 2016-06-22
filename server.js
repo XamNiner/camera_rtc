2f8310321758ae055738d12ec25505f7d45147c0
XamNiner <xamniner@fivedigits.net>
2016-06-22 10:16:46 +0200

    change server use

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, '/dist/');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
+  //.use((req, res) => res.sendFile(INDEX) )
+    .use(express.static(__dirname + '/dist/'));
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
